a553101e4c41ff05e9fa196cfad35f3713a70a9c
cauaosp <cauaosp20@gmail.com>
2025-03-04 14:54:54 -0300

    refactor: green icon to website

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
       <head>
         <title>cauaosp</title>
         <meta name="description" content="This is my website description." />
-        <link rel="icon" href="/icons/code-svgrepo.svg" type="image/svg+xml" />
+        <link rel="icon" href="/icons/code-svgrepo-green.svg" type="image/svg+xml" />
       </head>
       <body>
         <div className="grid gap-2 h-screen bg-gray-950 pt-2 md:px-10 select-none">
