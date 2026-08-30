3407b09f510482c5c281a70db09a0a4875cc54b0
cauaosp <cauaosp20@gmail.com>
2026-08-30 04:20:25 -0300

    adicionado background de bolinhas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { Hero } from "../components/sections/hero";
 
 export default function HomePage() {
   return (
-    <>
+    <main className="terminal-grid">
       <Hero />
-    </>
+    </main>
   );
 };
