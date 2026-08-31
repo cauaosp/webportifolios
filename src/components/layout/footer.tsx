42814ae6244184ce5c39393edda38a075567f3cf
cauaosp <cauaosp20@gmail.com>
2026-08-31 01:44:41 -0300

    refactor: adicionado link no meu nome no rodapé

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,14 +3,14 @@ import { Cat } from "@/src/components/icons";
 export const Footer = () => {
   return (
     <div className="mt-auto tracking-tight text-muted-foreground flex justify-center items-center w-full gap-3 py-5">
-      <div>© Desenvolvido por <span className="text-accent">Cauã OSP</span></div>
+      <div>© Desenvolvido por <a href="https://github.com/cauaosp" className="text-accent hover:text-foreground hover:cursor-pointer" target="_blank" rel="noreferrer">Cauã OSP</a></div>
       <div className="relative">
         <Cat
           width={20}
           height={20}
           color={"var(--accent)"}
         />
-        <div className="absolute bg-accent opacity-20 size-4 animate-pulse blur-sm top-1 left-0.5"></div>
+        <div className="absolute bg-accent opacity-30 size-4 animate-pulse blur-sm top-1 left-0.5"></div>
       </div>
     </div>
   );
